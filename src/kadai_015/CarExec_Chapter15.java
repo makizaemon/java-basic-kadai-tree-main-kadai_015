201fef2dd933cbc9544dfcdccf5164d9a2fa99c0
makizaemon <tainobu1226@gmail.com>
2026-01-13 10:37:32 +0900

    Update CarExec_Chapter15.java

--- src/kadai_015/CarExec_Chapter15.java
+++ src/kadai_015/CarExec_Chapter15.java
@@ -1,13 +1,10 @@
-package kadai_015;
-
 public class CarExec_Chapter15 {
-	public static void main(String[] args) { // mainメソッド（プログラムの開始点）
-
-        Car_Chapter15 car = new Car_Chapter15(); // Carクラスのインスタンスを生成
-
-        System.out.println("ギア1から3に切り替えました"); // ギア変更メッセージを表示
-
-        car.changeGear(3); // ギアを3速に変更
-        car.run();         // 変更後の速度を表示
+    public static void main(String[] args) {
+        Car_Chapter15 car = new Car_Chapter15();
+        
+        car.setFuel(20);
+        car.run(5);
+        car.changeGear(3);
+        car.run(10);
     }
 }
